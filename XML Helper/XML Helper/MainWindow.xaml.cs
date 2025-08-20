aa6e07e8e6a516467ed8ad9c4cc3b902f3766489
Islam <Superuser@DESKTOP-I93DVCU>
2025-08-20 15:35:38 +0300

    Добавлены методы для теста при запуске приложения и для добавления атрибутов в xml файлы.

--- XML Helper/XML Helper/MainWindow.xaml.cs
+++ XML Helper/XML Helper/MainWindow.xaml.cs
@@ -1,24 +1,84 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace XML_Helper
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private readonly string _data1Path;
+        private readonly string _xsltFilePath;
+        private readonly string _employeesOutPath;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string inputDirPath = Path.GetFullPath(Path.Combine( "..", "..", "..", "..", "..", "input"));
+            _data1Path = Path.Combine(inputDirPath, "data1.xml");
+            _xsltFilePath = Path.Combine(inputDirPath, "convert_to_employees.xslt");
+            _employeesOutPath = Path.Combine(inputDirPath, "employees.xml");
+            TestXsltConversion();
         }
+
+        private void TestXsltConversion()
+        {
+            try
+            {
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load(_xsltFilePath);
+                xslt.Transform(_data1Path, _employeesOutPath);
+
+                AddEmployeeSums();
+                AddData1Total();
+                MessageBox.Show("Преобразование и подсчёт сумм прошли успешно!\nФайл создан: " + _employeesOutPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка XSLT: " + ex.Message);
+            }
+        }
+
+        private void AddEmployeeSums()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(_employeesOutPath);
+
+            foreach (XmlNode emp in doc.SelectNodes("//Employee"))
+            {
+                double total = 0;
+                foreach (XmlNode salary in emp.SelectNodes("salary"))
+                {
+                    string amountStr = salary.Attributes["amount"].Value.Replace(',', '.');
+                    total += double.Parse(amountStr, System.Globalization.CultureInfo.InvariantCulture);
+                }
+                XmlAttribute totalAttr = doc.CreateAttribute("total");
+                totalAttr.Value = total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+                emp.Attributes.Append(totalAttr);
+            }
+
+            doc.Save(_employeesOutPath);
+        }
+
+        private void AddData1Total()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(_data1Path);
+            XmlNode payNode = doc.SelectSingleNode("//Pay");
+            double total = 0;
+            foreach (XmlNode item in doc.SelectNodes("//Pay/item"))
+            {
+                string amountStr = item.Attributes["amount"].Value.Replace(',', '.');
+                total += double.Parse(amountStr, System.Globalization.CultureInfo.InvariantCulture);
+            }
+
+            XmlAttribute totalAttr = doc.CreateAttribute("total");
+            totalAttr.Value = total.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+            payNode.Attributes.Append(totalAttr);
+            doc.Save(_data1Path);
+        }
+
     }
 }
--- input/convert_to_employees.xslt
+++ input/convert_to_employees.xslt
@@ -1,21 +1,17 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0"
-    xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:key name="by-person" match="item" use="concat(@surname,'|',@name)"/>
 
-  <xsl:output method="xml" indent="yes" encoding="UTF-8"/>
+	<xsl:output method="xml" indent="yes"/>
 
-  <xsl:template match="/">
-    <Employees>
-      <xsl:for-each select="Pay/item[generate-id() = generate-id(key('by-person', concat(@surname,'|',@name))[1])]">
-        <Employee surname="{@surname}" name="{@name}">
-          <xsl:for-each select="key('by-person', concat(@surname,'|',@name))">
-            <salary mount="{@mount}" amount="{@amount}"/>
-          </xsl:for-each>
-        </Employee>
-      </xsl:for-each>
-    </Employees>
-  </xsl:template>
-
-  <xsl:key name="by-person" match="item" use="concat(@surname,'|',@name)"/>
-
+	<xsl:template match="/Pay">
+		<Employees>
+			<xsl:for-each select="item[generate-id() = generate-id(key('by-person', concat(@surname,'|',@name))[1])]">
+				<Employee surname="{@surname}" name="{@name}">
+					<xsl:for-each select="key('by-person', concat(@surname,'|',@name))">
+						<salary mount="{@mount}" amount="{@amount}"/>
+					</xsl:for-each>
+				</Employee>
+			</xsl:for-each>
+		</Employees>
+	</xsl:template>
 </xsl:stylesheet>
